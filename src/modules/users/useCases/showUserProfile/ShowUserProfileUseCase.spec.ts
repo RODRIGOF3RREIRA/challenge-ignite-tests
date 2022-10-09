eecf0e7a13ece89e85a4e4be14812dfae85ffa14
RODRIGOF3RREIRA <rodrigoferreira2799@gmail.com>
2022-10-09 19:59:34 -0400

    test: should be able to show profile an user

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,54 @@
+import { verify } from "jsonwebtoken";
+import authConfig from "../../../../config/auth";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { AuthenticateUserUseCase } from "../authenticateUser/AuthenticateUserUseCase";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { ShowUserProfileError } from "./ShowUserProfileError";
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+let inMemoryUsersRepository: InMemoryUsersRepository;
+let sut: ShowUserProfileUseCase;
+let createUserUseCase: CreateUserUseCase;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+
+const { secret } = authConfig.jwt;
+
+export const makeUser = {
+  email: "test@test.com",
+  name: "user test",
+  password: "12345",
+};
+
+interface IPayload {
+  sub: string;
+}
+describe("Show User Profile Use Case", () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+    sut = new ShowUserProfileUseCase(inMemoryUsersRepository);
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      inMemoryUsersRepository
+    );
+  });
+
+  it("should be able to show profile an user", async () => {
+    await createUserUseCase.execute(makeUser);
+
+    const userAuthenticated = await authenticateUserUseCase.execute({
+      email: makeUser.email,
+      password: makeUser.password,
+    });
+
+    const { sub: user_id } = verify(
+      userAuthenticated.token,
+      secret
+    ) as IPayload;
+
+    const result = await sut.execute(user_id);
+
+    console.log(result);
+
+    expect(result).toHaveProperty("id");
+    expect(result.email).toBe(makeUser.email);
+    expect(result.name).toBe(makeUser.name);
+  });
