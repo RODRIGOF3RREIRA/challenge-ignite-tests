f57cb0ea5f4d3633b719d080926dc0b33ab898fd
RODRIGOF3RREIRA <rodrigoferreira2799@gmail.com>
2022-10-12 16:28:23 -0400

    test(integration): should be able to create a new withdraw

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -61,3 +61,37 @@ describe("Create Statement Controller", () => {
 
     expect(response.status).toBe(401);
   });
+
+  it("should be able to create a new withdraw", async () => {
+    const responseToken = await request(app).post("/api/v1/sessions").send({
+      email: makeUser.email,
+      password: makeUser.password,
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post("/api/v1/statements/deposit")
+      .send({
+        amount: 200,
+        description: "deposit test",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    const response = await request(app)
+      .post("/api/v1/statements/withdraw")
+      .send({
+        amount: 100,
+        description: "withdraw test",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
+    expect(response.body.amount).toBe(100);
+    expect(response.body.description).toBe("withdraw test");
+    expect(response.body).toHaveProperty("id");
+  });
